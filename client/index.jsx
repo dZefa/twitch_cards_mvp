6609ea5b6b7b9fac361f0613772c80ecebc733a9
Daniel Chong <danchong625@gmail.com>
2018-01-12 13:29:38 -0800

    [feat] initializes react index

---
+++ client/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+
+import { configureStore, history } from './src/store/configureStore';
+
+import App from './src/app.jsx';
+
+const store = configureStore();
+
+render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+);
