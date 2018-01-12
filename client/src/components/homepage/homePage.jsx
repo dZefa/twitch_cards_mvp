0696883c2049400c382432f5acae7f19b53e8278
Daniel Chong <danchong625@gmail.com>
2018-01-12 13:31:34 -0800

    [feat] initializes home page view

---
+++ client/src/components/homepage/homePage.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        This is HomePage;
+      </div>
+    )
+  }
+};
+
+export default HomePage;
