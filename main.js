c8edf2e14dd1f308827b818650c6e08bd62f8c1d
Daniel Chong <danchong625@gmail.com>
2018-01-13 00:24:17 -0800

    [ops] adds twitch oauth login with new window

--- main.js
+++ main.js
@@ -1,16 +1,36 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain, net } = require('electron');
 
 app.on('ready', () => {
   let mainWindow = new BrowserWindow({ width: 1280, height: 800 });
 
   mainWindow.loadURL(`file://${__dirname}/dist/index.html`);
 
+  mainWindow.webContents.openDevTools();
+
   mainWindow.webContents.on('did-finish-load', () => {
     if (!mainWindow) {
       throw new Error('"mainWindow" is not defined');
     }
-  mainWindow.show();
-  mainWindow.focus();
+    mainWindow.show();
+    mainWindow.focus();
+  });
+
+  mainWindow.webContents.on('new-window', (event, url) => {
+    event.preventDefault();
+
+    if (url === 'http://localhost:3000/auth/twitch') {
+      let twitchLogin = new BrowserWindow();
+  
+      twitchLogin.loadURL(url);
+      
+      event.newGuest = twitchLogin;
+  
+      twitchLogin.on('closed', () => {
+        twitchLogin = null;
+        mainWindow.webContents.send('twitchAuth');
+      });
+    }
+
   });
 
   mainWindow.on('closed', () => {
