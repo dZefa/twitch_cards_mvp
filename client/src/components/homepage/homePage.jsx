85bf5f19d6702bb0761363afeca22c37ec7f67c2
Daniel Chong <danchong625@gmail.com>
2018-01-13 00:25:44 -0800

    [feat] adds twitch auth

--- client/src/components/homepage/homePage.jsx
+++ client/src/components/homepage/homePage.jsx
@@ -1,17 +1,48 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { ipcRenderer } from 'electron';
+
+import * as authActions from '../../action/authAction';
 
 class HomePage extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
   }
 
   render() {
+    const { authorized, displayName, actions } = this.props;
+
+    ipcRenderer.on('twitchAuth', (event, args) => {
+      actions.checkAuth();
+    });
+
+    if (authorized === null) {
+      actions.checkAuth();
+    }
+    
     return (
       <div>
-        This is HomePage;
+        { authorized && <div>This is display name: {displayName}</div>}
+        <a href="http://localhost:3000/auth/twitch" target="_blank">
+          <img src="http://ttv-api.s3.amazonaws.com/assets/connect_dark.png" alt="twitch login" />
+        </a>
       </div>
     )
   }
 };
 
-export default HomePage;
+const HomeState = (state) => {
+  return {
+    authorized: state.auth.authorized,
+    displayName: state.auth.displayName,
+  }
+};
+
+const HomeDispatch = (dispatch) => {
+  return {
+    actions: bindActionCreators(authActions, dispatch),
+  }
+};
+
+export default connect(HomeState, HomeDispatch)(HomePage);
