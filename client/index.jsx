e3b496b52d8439c2a3a7f21dc1761aee2f5c5990
Daniel Chong <danchong625@gmail.com>
2018-01-13 19:49:29 -0800

    [feat] imports global css and bootstrap js

--- client/index.jsx
+++ client/index.jsx
@@ -7,6 +7,9 @@ import { configureStore, history } from './src/store/configureStore';
 
 import App from './src/app.jsx';
 
+import './src/app.global.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
+
 const store = configureStore();
 
 render(
