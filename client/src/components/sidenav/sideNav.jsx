413a133d6800f21ffa5640300b7112a9de973a6e
Daniel Chong <danchong625@gmail.com>
2018-01-14 15:49:47 -0800

    [feat] initializes side bar navigation

---
+++ client/src/components/sidenav/sideNav.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class SideNav extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        side nav
+      </div>
+    )
+  }
+};
+
+export default SideNav;
