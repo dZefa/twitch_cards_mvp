bc440b3d4879dce809455dfdc7207a94a3bff727
Daniel Chong <danchong625@gmail.com>
2018-01-13 21:39:04 -0800

    [feat] adds icon to close app and sends msg to electron

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { ipcRenderer } from 'electron';
 
 import './navbar.css';
 
@@ -9,10 +10,27 @@ class Navbar extends Component {
 
   }
 
+  closeApp() {
+    ipcRenderer.sendSync('closeApp');
+  }
+
   render() {
+    const { displayName } = this.props;
+
     return (
       <div id="navbar-page" className="nav-height">
-        navbar here!
+        <div className="row align-items-center fill-height">
+          <div className="col-2">
+            <h3>{displayName}</h3>
+          </div>
+          <div className="col-10 fill-height">
+            <div className="row justify-content-end align-items-start" id="nav-row-right">
+              <span onClick={() => this.closeApp()} >
+                <i className="fas fa-times" ></i>
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
