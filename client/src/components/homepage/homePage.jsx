eaff070439fa984edbdce4b0a377b30a7a08c5ab
Daniel Chong <danchong625@gmail.com>
2018-01-13 18:36:37 -0800

    [feat] updates to reflect addition of landing page

--- client/src/components/homepage/homePage.jsx
+++ client/src/components/homepage/homePage.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { ipcRenderer } from 'electron';
 
 import * as authActions from '../../action/authAction';
 
@@ -11,22 +10,11 @@ class HomePage extends Component {
   }
 
   render() {
-    const { authorized, displayName, actions } = this.props;
-
-    ipcRenderer.on('twitchAuth', (event, args) => {
-      actions.checkAuth();
-    });
-
-    if (authorized === null) {
-      actions.checkAuth();
-    }
+    const { displayName, actions } = this.props;
     
     return (
       <div>
-        { authorized && <div>This is display name: {displayName}</div>}
-        <a href="http://localhost:3000/auth/twitch" target="_blank">
-          <img src="http://ttv-api.s3.amazonaws.com/assets/connect_dark.png" alt="twitch login" />
-        </a>
+        Welcome back Coomahndah, {displayName}!
       </div>
     )
   }
