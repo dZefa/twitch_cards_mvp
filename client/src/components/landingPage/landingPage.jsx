3f3b838ad449ebcb5501c629b5a6f6edfa41ac1b
Daniel Chong <danchong625@gmail.com>
2018-01-13 20:20:13 -0800

    [feat] imports style and refactors landing page view to separate file

--- client/src/components/landingPage/landingPage.jsx
+++ client/src/components/landingPage/landingPage.jsx
@@ -3,7 +3,10 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { ipcRenderer } from 'electron';
 
+import './landingPage.css';
+
 import Loading from '../loading/loading.jsx';
+import LandView from './landingView.jsx';
 
 import * as authActions from '../../action/authAction';
 
@@ -23,19 +26,19 @@ class Landing extends Component {
   render() {
     const { authorized } = this.props;
 
-    if (authorized) {
-      return (
-        <Loading pathTo={'home'} />
-      )
-    } else {
-      return (
-        <div>
-          <a href="http://localhost:3000/auth/twitch" target="_blank">
-            <img src="http://ttv-api.s3.amazonaws.com/assets/connect_dark.png" alt="twitch login" />
-          </a>
+    return (
+      <div className="container fill-height" id="landing"> 
+        <div className="row align-items-center fill-height">
+          <div className="col align-self-center">
+            { 
+              authorized ? 
+              <Loading pathTo="home" /> :
+              <LandView />
+            }
+          </div>
         </div>
-      )
-    }
+      </div>
+    )
 
   }
 };
