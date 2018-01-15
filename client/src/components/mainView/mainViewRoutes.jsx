6df87e1011bac48c12f5f3c7942cf55e5ac45dad
Daniel Chong <danchong625@gmail.com>
2018-01-14 17:38:30 -0800

    [ops] adds account page route

--- client/src/components/mainView/mainViewRoutes.jsx
+++ client/src/components/mainView/mainViewRoutes.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from '../homepage/homePage.jsx';
-import MainView from './mainView.jsx';
+import AccPage from '../accountPage/accPage.jsx';
 
 class MainViewRoutes extends Component {
   constructor() {
@@ -13,6 +13,7 @@ class MainViewRoutes extends Component {
     return (
       <Switch>
         <Route exact path='/main' component={HomePage} />
+        <Route path='/main/acc' component={AccPage} />
       </Switch>
     )
   }
