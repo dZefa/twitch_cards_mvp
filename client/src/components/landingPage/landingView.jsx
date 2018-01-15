fc3d7e793f4682f9c9d7ac13f7018bf301723aa7
Daniel Chong <danchong625@gmail.com>
2018-01-15 14:24:00 -0800

    [feat] adds handle for failed login

--- client/src/components/landingPage/landingView.jsx
+++ client/src/components/landingPage/landingView.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const LandView = () => (
+const LandView = ({ authorized }) => (
   <div>
     <h1>TWITCH CARDS</h1>
-    <br />
+    { authorized === false ? <p style={{ color: 'red' }}>Login Failed!</p> : <br /> }
     <a href="http://localhost:3000/auth/twitch" target="_blank">
       <img src="http://ttv-api.s3.amazonaws.com/assets/connect_dark.png" alt="twitch login" />
     </a>
