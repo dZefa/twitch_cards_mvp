2f918c7a9f897dd9736dfbe5a4f81fcbb11ff3cd
Daniel Chong <danchong625@gmail.com>
2018-01-14 17:54:53 -0800

    [feat] implements redux and displays basic info from twitch

--- client/src/components/accountPage/accPage.jsx
+++ client/src/components/accountPage/accPage.jsx
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import './accPage.css';
 
 class AccPage extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
   }
 
   render() {
+    const { bio, displayName, email, logo, partnered, type } = this.props;
     return (
-      <div style={{ color: 'black' }}>
-        account!
+      <div id="acc-page">
+        <img src={logo} />
+        <p>{ partnered ? 'Partnered' : 'Viewer' }</p>
+        <p>Username: {displayName}</p>
+        <p>Bio: {bio}</p>
+        <p>Email: {email}</p>
+        <p>Type: {type}</p>
       </div>
     )
   }
 };
 
-export default AccPage;
+const AccState = (state) => {
+  return {
+    displayName: state.auth.displayName,
+    email: state.auth.email,
+    bio: state.auth.bio,
+    type: state.auth.type,
+    partnered: state.auth.partnered,
+    logo: state.auth.logo,
+  };
+};
+
+export default connect(AccState)(AccPage);
