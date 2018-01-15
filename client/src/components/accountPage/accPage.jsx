e8128ce2a845fd7a1e3539c06bdcaed6c12da51d
Daniel Chong <danchong625@gmail.com>
2018-01-14 17:37:45 -0800

    [feat] initializes account page

---
+++ client/src/components/accountPage/accPage.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class AccPage extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div style={{ color: 'black' }}>
+        account!
+      </div>
+    )
+  }
+};
+
+export default AccPage;
