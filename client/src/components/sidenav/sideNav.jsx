e45e56509af7f6bef6f64b54b151c9cee6dbeba5
Daniel Chong <danchong625@gmail.com>
2018-01-14 17:39:43 -0800

    [feat] integrates router with redux

--- client/src/components/sidenav/sideNav.jsx
+++ client/src/components/sidenav/sideNav.jsx
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link, withRouter } from 'react-router-dom';
 
 import './sideNav.css';
 
 class SideNav extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
   }
 
   render() {
+    const { actions } = this.props;
+
     return (
       <div className="col" id="side-nav">
-        side nav
+        <div>
+          <p><Link to='/main'>HOME</Link></p>
+          <p><Link to='/main/acc'>ACCOUNT</Link></p>
+        </div>
       </div>
     )
   }
 };
 
-export default SideNav;
+export default withRouter(connect()(SideNav));
