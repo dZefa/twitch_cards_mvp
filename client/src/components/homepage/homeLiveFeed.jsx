ca324838e568932abe548b286c8c0541e7d6fa5b
Daniel Chong <danchong625@gmail.com>
2018-01-15 22:18:51 -0800

    [feat] initialize live feed component

---
+++ client/src/components/homepage/homeLiveFeed.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class LiveFeed extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="col fill-height" id="live-feed">
+        live feed
+      </div>
+    )
+  }
+};
+
+export default LiveFeed;
