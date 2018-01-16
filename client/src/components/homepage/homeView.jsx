e7b78e0aa08d1d45c6de05781e5a47970309d270
Daniel Chong <danchong625@gmail.com>
2018-01-15 22:19:19 -0800

    [feat] initialize home view component

---
+++ client/src/components/homepage/homeView.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class HomeView extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="col fill-height" id="home-view">
+        home view
+      </div>
+    )
+  }
+};
+
+export default HomeView;
